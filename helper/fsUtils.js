636ac4489306d05946a17442710ad4cd291fd062
Dallon Lahoda <120769113+boomshakazulu@users.noreply.github.com>
2023-11-09 00:28:10 -0700

    Update fsUtils.js

--- helper/fsUtils.js
+++ helper/fsUtils.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const util = require('util');
 
 const readFromFile = util.promisify(fs.readFile);
-
+//promise to read the file
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-
+//reads and appends new notes. parsing the db json, pushing the new note and writing it to the new db file
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
